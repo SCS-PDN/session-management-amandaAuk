40621741767b199ab7b00c93f857765fd7abbc66
amandaAuk <amandaudara820@gmail.com>
2025-05-02 15:43:51 +0530

    Update LoginServlet.java

--- LoginServlet.java
+++ LoginServlet.java
@@ -1,19 +1,30 @@
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+    private static final Map<String, String> USERS = new HashMap<>();
+    static {
+        USERS.put("student1", "pass123");
+        USERS.put("student2", "pass456");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // TODO: Implement login logic
-        // 1. Get username & password from request
-        // 2. Validate credentials (hardcode a few users)
-        // 3. If valid:
-        //    - Create session
-        //    - Store username in cookie
-        //    - Redirect to DashboardServlet
-        // 4. If invalid, redirect back to login.html
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (username != null && password != null && password.equals(USERS.get(username))) {
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+            session.setAttribute("enrolledCourses", new java.util.ArrayList<String>());
+            response.sendRedirect("DashboardServlet");
+        } else {
+            response.sendRedirect("login.html?error=Invalid+Credentials");
+        }
     }
 }
